3c3984d227dd4d0c374f4ddcefa1c218219099dc
JOSE CURIEL <josecuriel88@gmail.com>
2020-09-18 00:38:44 -0500

    Test arrendatario
    Test arrendatario

---
+++ ws-inmo-mother/src/test/java/com/thali/ws/inmo/mother/repository/ArrendatarioRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.thali.ws.inmo.mother.repository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ArrendatarioRepositoryTest {
+
+	@Autowired 
+	private ArrendatarioRepository arrendatarioRepository;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testSave() throws Exception {
+	
+		
+	}
+
+}
