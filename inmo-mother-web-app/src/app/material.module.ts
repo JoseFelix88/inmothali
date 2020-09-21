bb83bfb5713131907f38192a57edece20581b619
JOSE CURIEL <josecuriel88@gmail.com>
2020-09-20 21:55:00 -0500

    cliente frontend
    cliente frontend - linea base.

---
+++ inmo-mother-web-app/src/app/material.module.ts
@@ -0,0 +1,75 @@
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatSelectModule} from '@angular/material/select';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDividerModule} from '@angular/material/divider';
+
+import {MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatTabsModule,
+        MatIconModule,
+        MatDialogModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatSnackBarModule
+} from '@angular/material';
+
+
+
+
+@NgModule({
+    imports:[
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatTabsModule,
+        MatIconModule,
+        MatSelectModule,
+        MatGridListModule,
+        MatDialogModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatSnackBarModule,
+        MatSidenavModule,
+        MatMenuModule,
+        MatTooltipModule,
+        MatDividerModule
+    ],
+    exports:[
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatTabsModule,
+        MatIconModule,
+        MatSelectModule,
+        MatGridListModule,
+        MatDialogModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatSnackBarModule,
+        MatSidenavModule,
+        MatMenuModule,
+        MatTooltipModule,
+        MatDividerModule
+    ]
+})
+
+export class MaterialModule{}
