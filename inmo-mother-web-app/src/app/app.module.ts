cccbf9bab990c560a9b63d09f0732313e38c2880
JoseFelix88 <josecuriel88@gmail.com>
2020-09-24 23:46:02 -0500

    Integracion de bootstrap y adicion de formularios
    Integracion de bootstrap y adicion de formularios reactivos.

--- inmo-mother-web-app/src/app/app.module.ts
+++ inmo-mother-web-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,7 +16,8 @@ import { LoginComponent } from './usuario/login/login.component';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- inmo-mother-web-app/src/app/usuario/login/login.component.ts
+++ inmo-mother-web-app/src/app/usuario/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  public formGroupLogin: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuildLogin: FormBuilder) { }
+  ngOnInit(): void {
+    this.buildFormLogin();
+  }
+
+  private buildFormLogin() {
+    this.formGroupLogin = this._formBuildLogin.group({
+      username: ['', [ 
+        Validators.required, Validators.minLength(6)]],
+      password: ['', [
+        Validators.required, Validators.minLength(4)]]
+    });
+  }
+
+  /**
+   * login
+   */
+  public login() {
+    console.log(this.formGroupLogin.value);
+  }
 
 }
